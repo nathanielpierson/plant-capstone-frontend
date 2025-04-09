e0e38ef13fc5bf1949e09d7d169c342f02a0425b
nathanielpierson <nathanielepierson@gmail.com>
2025-04-09 17:48:04 -0400

    testing time components

--- src/SchedulesShow.jsx
+++ src/SchedulesShow.jsx
@@ -8,6 +8,14 @@ export function SchedulesShow({ schedules }) {
           <p>
             plant is {schedule.plant.name} belongs to user {schedule.user.name}
           </p>
+          <p>
+            {schedule.plant.name} needs watered every{" "}
+            {schedule.plant.days_to_water} days
+          </p>
+          <p>
+            needs watered in: {schedule.plant.days_to_water} plus current
+            datetime minus {schedule.watering_start_date}
+          </p>
         </div>
       ))}
     </div>
