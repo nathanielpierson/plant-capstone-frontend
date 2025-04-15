74d68848238beaffc94e72d1001c17560da56f64
nathanielpierson <nathanielepierson@gmail.com>
2025-04-14 22:50:29 -0400

    progress bar added

--- src/SchedulesShow.jsx
+++ src/SchedulesShow.jsx
@@ -5,7 +5,7 @@ export function SchedulesShow({ schedules, onWatered, onDestroy, onUpdate }) {
   useEffect(onUpdate, []);
   return (
     <div>
-      <h1>all schedules</h1>
+      <h1>all of your schedules</h1>
       {schedules.map((schedule) => (
         <div key={schedule.id}>
           <img src={schedule.plant.image_url} height="200" width="300" />
@@ -14,6 +14,16 @@ export function SchedulesShow({ schedules, onWatered, onDestroy, onUpdate }) {
             {schedule.user.name}
           </p>
           <h3>started watering at {schedule.created_at}</h3>
+          <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              style={{width: `${Math.round((schedule.growth_status / schedule.plant.growth_req) * 100)}%`}}
+              aria-valuenow={`${Math.round((schedule.growth_status / schedule.plant.growth_req) * 100)}%`}
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >Growth Status: stage {schedule.growth_status} out of {schedule.plant.growth_req}</div>
+          </div>
           <p>
             {schedule.plant.name} needs watered every{" "}
             {schedule.plant.days_to_water} days
@@ -22,6 +32,11 @@ export function SchedulesShow({ schedules, onWatered, onDestroy, onUpdate }) {
           <p>
             Needs watered in{" "}
             {schedule.plant.days_to_water * 24 - schedule.time_changed} hours
+          </p>
+          <p>
+            {Math.round(
+              (schedule.growth_status / schedule.plant.growth_req) * 100
+            )}
           </p>
           <div>
             {schedule.plant.days_to_water * 24 - schedule.time_changed > 0 ? (
