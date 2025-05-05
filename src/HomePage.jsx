e9867a4d1e65be7639b413b91bbfbde6b987220e
nathanielpierson <nathanielepierson@gmail.com>
2025-05-05 13:05:48 -0400

    add logo to home page and css style for opacity hover

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -4,7 +4,10 @@ export function HomePage() {
   if (localStorage.jwt) {
     return (
       <div>
-        <h1>Welcome to plants app!</h1>
+        <h1>Welcome to &nbsp;
+        <img src="/Plant-app-logo.png" height="60" width="60" className="inline-block hover-opacity" alt="Plant App Logo"/>
+        &nbsp; app!</h1>
+        <br></br>
         <Link to="/schedules"> Your plant schedules </Link>
       </div>
     );
--- src/SchedulesIndex.jsx
+++ src/SchedulesIndex.jsx
@@ -4,7 +4,7 @@ export function SchedulesIndex({ schedules }) {
       <h1>all schedules</h1>
       {schedules.map((schedule) => (
         <div key={schedule.id}>
-          <img src={schedule.plant.image_url} height="200" width="300" />
+          <img src={schedule.plant.image_url} className="hover-opacity" height="200" width="300" />
           <p>
             plant is {schedule.plant.name} belongs to user {schedule.user.name}
           </p>
