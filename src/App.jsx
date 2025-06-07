d664448883bfbb0aa1780e204bd55bb81f1a6a55
nathanielpierson <nathanielepierson@gmail.com>
2025-06-07 12:13:02 -0400

    fix inconsistenvy in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import "./App.css";
 import AuthProvider from "react-auth-kit";
 import createStore from "react-auth-kit/createStore";
 
-axios.defaults.baseURL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "<your-backend-url>";
+axios.defaults.baseURL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://plant-rebuild-api.onrender.com";
 
 const store = createStore({
   authName: "_auth",
