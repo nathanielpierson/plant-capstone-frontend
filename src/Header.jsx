4712c15692741c999d74e446645dedb11f14cf49
nathanielpierson <nathanielepierson@gmail.com>
2025-06-07 11:02:53 -0400

    configure axios with development/production conditional

--- src/Header.jsx
+++ src/Header.jsx
@@ -14,6 +14,7 @@ export function Header() {
 
   return (
     <div>
+      I have no idea how this pineapple got here
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
         <a href="/profile">
